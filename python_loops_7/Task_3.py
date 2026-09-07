9a9275c71300d525732a914586ae065442036814
Артём Кобылинский <art_kob@mail.ru>
2026-09-07 11:39:25 +0300

    Task 3 FIXED

--- python_loops_7/Task_3.py
+++ python_loops_7/Task_3.py
@@ -1,13 +1,13 @@
 # Конфигурационный словарь, полученный от сервиса инициализации
 db_config = {
-"connection": {
-"host": "production-db.internal",
-"port": 5432,
-"user": "postgres",
-"ssl_setting":{
-    "ssl_mode":""
-}
-}
+    "connection": {
+        "host": "production-db.internal",
+        "port": 5432,
+        "user": "postgres"
+    },
+    "ssl_setting": {
+        "ssl_mode": ""
+    }
 }
 # Ваш код здесь
 connection = db_config["connection"]
@@ -16,7 +16,7 @@ db_host = connection["host"]
 
 db_port = connection["port"]
 
-db_ssl_settings = connection.get("ssl_setting",{})
+db_ssl_settings = db_config.get("ssl_settings",{})
 db_ssl_mode = db_ssl_settings.get("ssl_mode","verify-full")
 
 connection["user"]="admin"
@@ -25,3 +25,5 @@ connection["max_connections"] = "100"
 
 for key, value in connection.items():
     print(f"{key}: {value}")
+
+print("SSL Mode: ", db_ssl_mode)
